361a08db362c4a704389badf804383f87cf6f142
Gokula kannan <agmgokul46@gmail.com>
2025-11-10 14:35:41 +0530

    I added navbar lists

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Nav</div>;
+  return (
+    <div>
+      <li>Home</li>
+      <li>About</li>
+      <li>services</li>
+      <li>Contact Us</li>
+    </div>
+  )
 };
 
 export default Navbar;
